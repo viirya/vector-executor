200c92332c5617254da34713db3a13be720f4068
Liang-Chi Hsieh <viirya@gmail.com>
2021-11-03 13:04:43 -0700

    Add jni_api crate.

---
+++ src/main/rust/vector-executor/src/jni_api.rs
@@ -0,0 +1,52 @@
+//! Define JNI APIs which can be called from Java/Scala.
+
+use jni::objects::{JClass, JString};
+use jni::sys::{jint, jlong};
+use jni::JNIEnv;
+
+#[no_mangle]
+/// Test JNI API. Accept a Java string and return it as an integer.
+pub extern "system" fn Java_org_viirya_vector_native_VectorLib_test(
+    env: JNIEnv,
+    _class: JClass,
+    input: JString,
+) -> jint {
+    let input: String = env
+        .get_string(input)
+        .expect("Couldn't get java string!")
+        .into();
+
+    let output = input.parse::<i32>().unwrap();
+
+    output
+}
+
+#[no_mangle]
+/// Test JNI API. Accept an address of OffHeapColumnVector of Spark, access its value and return.
+pub extern "system" fn Java_org_viirya_vector_native_VectorLib_passOffHeapVector(
+    _env: JNIEnv,
+    _class: JClass,
+    address: jlong,
+    _num_row: jint,
+) -> jint {
+    /*
+    let raw_address: *mut [i32] = address as *mut [i32];
+
+    let values = unsafe {
+        Box::<[i32]>::from_raw(raw_address)
+    };
+    values[0].count_zeros() as i64
+     */
+
+    let output = unsafe { *(address as *mut i32) };
+    output
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
--- src/main/rust/vector-executor/src/lib.rs
+++ src/main/rust/vector-executor/src/lib.rs
@@ -1,51 +1,8 @@
-use jni::objects::{JClass, JString, JValue};
-use jni::sys::{jint, jlong, jlongArray, jstring};
-use jni::JNIEnv;
+//! PoC of vectorization execution through JNI to Rust.
+#![deny(warnings)]
+#![deny(missing_docs)]
 
-use std::boxed::Box;
-use std::env;
-use std::fs::File;
-use std::mem;
-use std::ops::Deref;
-use std::sync::Arc;
-use std::time::Instant;
-use std::vec::Vec;
-
-#[no_mangle]
-pub extern "system" fn Java_org_viirya_vector_native_VectorLib_test(
-    env: JNIEnv,
-    class: JClass,
-    input: JString,
-) -> jint {
-    let input: String = env
-        .get_string(input)
-        .expect("Couldn't get java string!")
-        .into();
-
-    let output = input.parse::<i32>().unwrap();
-
-    output
-}
-
-#[no_mangle]
-pub extern "system" fn Java_org_viirya_vector_native_VectorLib_passOffHeapVector(
-    env: JNIEnv,
-    class: JClass,
-    address: jlong,
-    num_row: jint,
-) -> jint {
-    /*
-    let raw_address: *mut [i32] = address as *mut [i32];
-
-    let values = unsafe {
-        Box::<[i32]>::from_raw(raw_address)
-    };
-    values[0].count_zeros() as i64
-     */
-
-    let output = unsafe { *(address as *mut i32) };
-    output
-}
+pub mod jni_api;
 
 #[cfg(test)]
 mod tests {
